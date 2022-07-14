736a90ef2122834ebceeb47be148f8dcee3bbe26
Kiran Yadav <59909594+kiran5391@users.noreply.github.com>
2022-07-14 16:43:23 +0530

    Add files via upload

--- Java Lab Work/Assignment_14_July/ComparisionOfElementsInArraylist.java
+++ Java Lab Work/Assignment_14_July/ComparisionOfElementsInArraylist.java
@@ -1,5 +1,5 @@
+/* Accept two arraylist and check all the elements of second list is present in the first list or not*/
 import java.util.ArrayList;
-
 public class ComparisionOfElementsInArraylist 
 {
 	public static void main(String args[])
@@ -11,14 +11,16 @@ public class ComparisionOfElementsInArraylist
 		a2.add("Pooja");
 		a2.add("Humaira");
 		a2.add("Neha");
-		System.out.println(a2);
-		a1.add("Afsha");
-		a1.add("Pooja");
-		a1.add("Humaira");
-		a1.add("Neha");
+		System.out.println("Arraylist 2:  "+a2);
+		a1.addAll(a2);
 		if(a1.containsAll(a2))
 		{
-			System.out.println(a1);
+			System.out.println();
+			System.out.println("Arraylist 1:  "+a1);
+		}
+		else
+		{
+			System.out.println("Arraylist 1 doesn't contain all elements that are present in arraylist 2");
 		}
 	}	
 }
